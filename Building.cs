eb1ff291fbfc549d87753fc21ebd8b89a8117abd
John Douglas Price <johndouglasprice@Johns-MacBook-Pro.local>
2021-04-20 09:37:05 -0400

    Part 1 Finished

---
+++ Building.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace Planner
+{
+    public class Building {
+        private string _designer = "John Price";
+        public string GetDesigner() 
+        {
+            return _designer;
+        }
+        private DateTime _dateConstructed { get; set; }
+        public DateTime GetTime()
+        {
+            return _dateConstructed;
+        }
+        private string _address { get; set; }
+        public string GetAddress() 
+        {
+            return _address;
+        }
+        private string _owner {get; set;}
+        public string GetOwner()
+        {
+            return _owner;
+        }
+
+        // Properties
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        // A readonly property
+        public double Volume 
+        {
+            get
+            { 
+                return Width * Depth * (3 * Stories);
+            }
+        }
+
+        // Constructor
+        public Building(string address) {
+            _address = address;
+        }
+
+        // Methods
+        public void Construct() 
+        {
+            _dateConstructed = DateTime.Now;
+        }
+
+        public void Purchase(string purchaseName) 
+        {
+            _owner = purchaseName;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Planner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building FiveOneTwoEigth = new Building("512 8th Avenue")
+            {
+                Stories = 12,
+                Width = 145.45,
+                Depth = 500.20
+            };
+
+            FiveOneTwoEigth.Construct();
+            FiveOneTwoEigth.Purchase("Timothy");
+
+            Console.WriteLine($"\n{FiveOneTwoEigth.GetAddress()}");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"Designed by {FiveOneTwoEigth.GetDesigner()}\nConstructed on {FiveOneTwoEigth.GetTime()}\nOwned by {FiveOneTwoEigth.GetOwner()}\n{Math.Round(FiveOneTwoEigth.Volume)} cubic meters of space\n");
+
+            Building TallBuilding = new Building("732 Washington Drive")
+            {
+                Stories = 500,
+                Width = 180,
+                Depth = 200000
+            };
+
+            TallBuilding.Construct();
+            TallBuilding.Purchase("Gregory");
+
+            Console.WriteLine($"\n{TallBuilding.GetAddress()}");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"Designed by {TallBuilding.GetDesigner()}\nConstructed on {TallBuilding.GetTime()}\nOwned by {TallBuilding.GetOwner()}\n{Math.Round(TallBuilding.Volume)} cubic meters of space\n");
+
+            Building ShortBuilding = new Building("94 Tacoma Rd")
+            {
+                Stories = 2,
+                Width = 100,
+                Depth = 20
+            };
+
+            ShortBuilding.Construct();
+            ShortBuilding.Purchase("Jimmy Diego");
+
+            Console.WriteLine($"\n{ShortBuilding.GetAddress()}");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"Designed by {ShortBuilding.GetDesigner()}\nConstructed on {ShortBuilding.GetTime()}\nOwned by {ShortBuilding.GetOwner()}\n{Math.Round(ShortBuilding.Volume)} cubic meters of space\n");
+        }
+    }
+}
